77131260f34f6a009ef18fa54b09de973a064873
scaw76 <scawley@dmail.dixie.edu>
2015-07-08 21:23:10 -0600

    High score stays on new game and game over fixed. Need high schore to stay on browser refresh.

--- 2048.js
+++ 2048.js
@@ -1,100 +1,12 @@
-//add save data function
-
-
 var boardSize = 4 ;
 var score = 0 ;
 var bestScore = 0;
 
 var board = {
-/*
-	"tile0-0": 2,
-	"tile1-0": 4,
-	"tile2-0": 8,
-	"tile3-0": 16
-
-	"tile0-1": 32,
-	"tile1-1": 64,
-	"tile2-1": 128,
-	"tile3-1": 256,
-
-	"tile0-2": 512,
-	"tile1-2": 1024,
-	"tile2-2": 2048, 
-	"tile3-2": 4096
-
-	"tile0-3":
-	"tile1-3":
-	"tile2-3":
-	"tile3-3":
-*/
 };
 
-var tileKey = function(col,row){
+var tileKey = function (col,row){
 	return "tile" + col + "-" + row;
-};
-
-var getrow = function(row){
-	var nums = [];
-	for(var col = 0 ; col<boardSize;col++){
-		var key = tileKey(col,row);
-		var val = board[key];
-		if(val){
-			nums.push(val);
-		}
-	}
-	return nums;
-}
-
-var getcol = function(col){
-	var nums = [];
-	for(var row = 0 ; row<boardSize;row++){
-		var key = tileKey(col,row);
-		var val = board[key];
-		if(val){
-			nums.push(val);
-		}
-	}
-	return nums;
-}
-
-var setrow = function(newnums, row){
-	for(var col = 0 ; col<boardSize;col++){
-		var key = tileKey(col,row);
-		board[key] = newnums[col];
-	}
-}
-
-var setcol = function(newnums, col){
-	for(var row = 0 ; row<boardSize;row++){
-		var key = tileKey(col,row);
-		board[key] = newnums[row];
-	}
-}
-
-var combine = function (numbers){
-	var newNumbers = [];
-	// combine
-		while(numbers.length>0){
-			if(numbers[0] === numbers[1]){
-				// add them together
-				var sum = numbers[0] + numbers[1]
-				score += sum;
-				// push sum onto newNumbers
-				newNumbers.push(sum);
-				// remove both numbers from numbers
-				numbers.shift();
-				numbers.shift();
-			} else{
-				// push first number onto newNumbers
-				newNumbers.push(numbers[0]);
-				//remove the first item from numbers array
-				numbers.shift();
-			}
-		}
-	while(newNumbers.length<boardSize){
-		newNumbers.push(undefined);
-	}
-	return newNumbers;
 };
 
 var createBoard = function(){
@@ -110,7 +22,75 @@ var createBoard = function(){
 	}
 };
 
-var resetBoard = function(){
+
+var getrow = function (row){
+	var nums = [];
+	for(var col = 0 ; col<boardSize;col++){
+		var key = tileKey(col,row);
+		var val = board[key];
+		if(val){
+			nums.push(val);
+		}
+	}
+	return nums;
+}
+
+var getcol = function (col){
+	var nums = [];
+	for(var row = 0 ; row<boardSize;row++){
+		var key = tileKey(col,row);
+		var val = board[key];
+		if(val){
+			nums.push(val);
+		}
+	}
+	return nums;
+}
+
+var setrow = function (newnums, row){
+	for(var col = 0 ; col<boardSize;col++){
+		var key = tileKey(col,row);
+		board[key] = newnums[col];
+	}
+}
+
+var setcol = function (newnums, col){
+	for(var row = 0 ; row<boardSize;row++){
+		var key = tileKey(col,row);
+		board[key] = newnums[row];
+	}
+}
+
+var combine = function (numbers){
+	var newNumbers = [];
+	// combine
+		while(numbers.length>0){
+			if(numbers[0] === numbers[1]){
+				// add them together
+				var sum = numbers[0] + numbers[1]
+				updateScore(sum);
+				// push sum onto newNumbers
+				newNumbers.push(sum);
+				// remove both numbers from numbers
+				numbers.shift();
+				numbers.shift();
+			} else {
+				// push first number onto newNumbers
+				newNumbers.push(numbers[0]);
+				//remove the first item from numbers array
+				numbers.shift();
+			}
+		}
+		// padd with zeros
+		while(newNumbers.length<boardSize){
+		newNumbers.push(undefined);
+	}
+	return newNumbers;
+};
+
+
+
+var refreshBoard = function(){
 	for(var row = 0; row < boardSize; row++){
 		for(var col = 0; col < boardSize; col++){
 			var key = tileKey(col,row);
@@ -120,10 +100,10 @@ var resetBoard = function(){
 			$tile.text(val).addClass("tile-" + val);
 		}
 	}
-	// update score
+	updateScore();
 };
 
-var combineBack = function(nums){
+var combineBack = function (nums){
 	return combine(nums.reverse()).reverse();
 };
 
@@ -133,7 +113,7 @@ var moveLeft = function (){
 		var newnum = combine(old);
 		setrow(newnum,n);
 	}
-	resetBoard();
+	refreshBoard();
 };
 
 var moveUp = function (){
@@ -181,7 +161,7 @@ var addTile = function(){
 	board[index] = twoorfour;
 };
 
-var hasChanged = function(oldBoard){
+var hasChanged = function (oldBoard){
 	for(var row = 0; row < boardSize; row++){
 		for(var col = 0; col < boardSize; col++){
 			var key = tileKey(col,row);
@@ -193,41 +173,13 @@ var hasChanged = function(oldBoard){
 	return false;
 };
 
-
-var newGame = function (){
-	board = {};
-	score = 0;
-	addTile();
-	addTile();		
-	resetBoard();
-	refreshScore();
-	// save data
-	$("#gameOver").remove();
-};
-
-var loadSavedData = function(){
-
-	var savedBestScore = localStorage.getItem("bestScore")
-	if(savedBestScore){
-		bestScore = savedBestScore;
-
-	}
-	var b = localStorage.getItem("board");
-	if(b){
-		board = JSON.parse(b);
-	}else{
-		newGame();
-	}
-	
-};
-
 var possibleCombination = function (){
 	for(var row = 0; row < boardSize; row++){
 		for(var col = 0; col < boardSize; col++){
 			var keyAt = tileKey(col,row);
 			var keyRight = tileKey(col+1,row);
 			var keyDown = tileKey(col,row+1);
-			if(board[keyAt]==board[keyRight] || board[keyAt]==board[keyDown]){
+			if(board[keyAt]===board[keyRight] || board[keyAt]===board[keyDown]){
 				return true;
 			}
 		}
@@ -235,46 +187,77 @@ var possibleCombination = function (){
 	return false;
 };
 
-var refreshScore = function(){
-	// cool animation?
-	var $score = $("#score");
-	var $bestScore = $("#bestScore")
-	$score.text("Score: " + score);
-	if(score>bestScore){
-		bestScore = score;
-		localStorage.setItem("bestScore",bestScore);
-	}
-	$bestScore.text("Best Score: " + bestScore);
-}
-
-var gameOverMessage = function (){
-	alert("Game Over!");
-	$("<div></div>").appendTo("#gameOver")
-					.text("Game Over!")
-					.attr("id","gameOver")
-					.hide()
-					.fadeIn();
-};
-
-var isOver = function (){
+var gameOver = function (){
 	var empty = getEmpty();
-	var f = ["tile1-1"];
-	var e = [];
-
-	if(empty[0]){
-		//console.log("empty spaces");
+	//var f = ["tile1-1"];
+	//var e = [];
+	if(empty.length>0){
 		return false;
-
 	}
 	else if(possibleCombination()){
 		//console.log("possible combination");
 		return false
 	}
-	gameOverMessage();
 	return true;
 };
 
-var keyPressed = function(dir){
+var saveData = function () {
+	localStorage.setItem("score", score);
+	localStorage.setItem("bestsScore", bestScore);
+	localStorage.setItem("board", JSON.stringify(board));
+};
+
+var updateScore = function (increment){
+	// cool animation?
+	if (increment){
+		score+=increment;
+	}
+	if(score>bestScore){
+		bestScore = score;
+	}
+	saveData();
+	$("#score").text(score);
+	$("#bestScore").text(bestScore);
+};
+
+var newGame = function (){
+	board = {};
+	score = 0;
+	addTile();
+	addTile();		
+	refreshBoard();
+	saveData();
+	$("#game-over").remove();
+};
+
+var loadSavedData = function(){
+	var savedScore = localStorage.getItem("score");
+	if(savedScore){
+		score = parseInt(savedScore,10);
+	}
+	var savedBestScore = localStorage.getItem("bestScore")
+	console.log(savedBestScore);
+	if(savedBestScore){
+		bestScore = savedBestScore;
+	}
+	var b = localStorage.getItem("board");
+	if(b){
+		board = JSON.parse(b);
+	} else {
+		newGame();
+	}
+};
+
+var gameOverMessage = function (){
+	//alert("Game Over!");
+	$("<div></div>").appendTo("#board")
+					.attr("id","game-over")
+					.text("Game Over!")
+					.hide()
+					.fadeIn();
+};
+
+var keyPressed = function (dir){
 	var oldBoard = $.extend({},board);
 	if(dir =="left"){
 		moveLeft();
@@ -286,13 +269,13 @@ var keyPressed = function(dir){
 		moveDown();
 	}
 
-	if(hasChanged(oldBoard)){
+	if(hasChanged(oldBoard)) {
 		addTile();
-		resetBoard();
-		localStorage.setItem("board", JSON.stringify(board));
-		refreshScore();
-		isOver();
-
+		refreshBoard();
+		saveData();
+		if (gameOver()) {
+			gameOverMessage();
+		}
 	}
 };
 
@@ -300,13 +283,13 @@ $(document).ready(function(){
 	createBoard();
 	//newGame();
 	loadSavedData();
-	resetBoard();
+	refreshBoard();
 	
 	$("#newGame").click(function(){
 		newGame();
 	})
 
-	$(document).keydown(function(e){
+	$(document).keydown(function (e){
 		//console.log("Pushed key. "+ e.which);
 		switch(e.which){
 			case 37://left
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2048
+Tile game.
+Get a tile up to 2048 to win.
